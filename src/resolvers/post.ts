6a46d1bf1ee34cdf8a21163925dc07c17df85046
kevin700brands <kevin@700brands.com>
2021-12-01 18:43:03 +0800

    Finds a single post by passing an ID in Graphql

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,11 +1,21 @@
 import { Post } from "../entities/Post";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
 import { MyContext } from "../types";
 
 @Resolver()
 export class PostResolver {
+  // query all posts
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  // finds a post by giving them an id
+  @Query(() => Post)
+  post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
   }
 }
