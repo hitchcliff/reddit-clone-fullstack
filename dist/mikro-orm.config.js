0891556945fe803a72d745dae254d1169ee45c93
kevin700brands <kevin@700brands.com>
2021-12-02 07:34:33 +0800

    Updated the migration for User

--- dist/mikro-orm.config.js
+++ dist/mikro-orm.config.js
@@ -6,8 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const constansts_1 = require("./constansts");
 const Post_1 = require("./entities/Post");
 const path_1 = __importDefault(require("path"));
+const User_1 = require("./entities/User");
 exports.default = {
-    entities: [Post_1.Post],
+    entities: [Post_1.Post, User_1.User],
     dbName: "lilredit",
     user: "postgres",
     password: "postgres",
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constansts";
 import { Post } from "./entities/Post";
 import path from "path";
+import { User } from "./entities/User";
 
 // get the types of the fn that has parameters
 type MikroOrmConfigTypes = Parameters<typeof MikroORM.init>;
 
 // configuration
 export default {
-  entities: [Post],
+  entities: [Post, User],
   dbName: "lilredit",
   user: "postgres",
   password: "postgres",
