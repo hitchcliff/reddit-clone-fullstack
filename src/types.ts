dc7b866cca72d2d6a4a2282c1a07a1efdab2b6a3
kevin700brands <kevin@700brands.com>
2021-12-02 09:11:29 +0800

    Configured Redis and Express session for Cookies

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,8 @@
 import { Connection, EntityManager, IDatabaseDriver } from "@mikro-orm/core";
+import { Request, Response } from "express";
 
-export interface MyContext {
+export type MyContext = {
   em: EntityManager<IDatabaseDriver<Connection>>;
-}
+  res: Response;
+  req: Request;
+};
