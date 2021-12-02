0faf903f95c19a31e5dee296531c8ea03695f3fa
kevin700brands <kevin@700brands.com>
2021-12-02 08:13:34 +0800

    Registered a user in the DB

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
   // connects to the db
@@ -20,7 +21,7 @@ const main = async () => {
   // run server through apollo
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
     context: () => ({ em: orm.em }),
---
+++ src/resolvers/user.ts
@@ -0,0 +1,33 @@
+import { MyContext } from "src/types";
+import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import argon2 from "argon2";
+import { User } from "../entities/User";
+
+// types
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@Resolver()
+export class UserResolver {
+  // registers a user
+  @Mutation(() => User)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<User> {
+    const hashedPassword = await argon2.hash(options.password); // hashed password
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+
+    await em.persistAndFlush(user);
+
+    return user;
+  }
+}
