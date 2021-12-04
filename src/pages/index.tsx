5b55e9f827040ab341a86f340d0b835269942a8e
kevin700brands <kevin@700brands.com>
2021-12-04 13:41:42 +0800

    Perform a cleanup to NextJS & Chakra

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import { Container } from "../components/Container";
+
+const Index = () => (
+  <Container height="100vh">
+    <DarkModeSwitch />
+    hello world
+  </Container>
+);
+
+export default Index;
