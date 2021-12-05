b33d81361b99fff43dabcbbe2f7466e07afdb410
kevin700brands <kevin@700brands.com>
2021-12-05 10:58:58 +0800

    Logged a user

---
+++ src/pages/login.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Form, Formik } from "formik";
+import { Wrapper } from "../components";
+import { InputField } from "../components/InputField";
+import { Box } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
+import { useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/dist/client/router";
+
+interface loginProps {}
+
+const login: React.FC<loginProps> = ({}) => {
+  const [, login] = useLoginMutation();
+  const router = useRouter();
+
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({ options: values });
+          const errors = response.data?.login.errors;
+
+          // if it has error
+          if (errors) {
+            setErrors(toErrorMap(errors));
+          } else if (response.data?.login.user) {
+            // if we have a user
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="Username"
+              label="Username"
+              type="text"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="Password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button mt={4} isLoading={isSubmitting} color="teal" type="submit">
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default login;
