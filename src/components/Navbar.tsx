f8916fa2c8c3a166ed74db6b49b9a05abbefbba8
kevin700brands <kevin@700brands.com>
2021-12-07 09:08:44 +0800

    Added Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,47 @@
 import { Flex, Box, Link } from "@chakra-ui/layout";
 import NextLink from "next/link";
 import React from "react";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavbarProps {}
 
 export const Navbar: React.FC<NavbarProps> = ({}) => {
-  return (
-    <Flex bg="teal" p={4} width="100%">
-      <Box ml="auto" color="white">
+  const [{ data, fetching }] = useMeQuery();
+
+  let body = null;
+
+  // data is loading
+  if (fetching) {
+    // user not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href="/login">
           <Link mr={2}>Login</Link>
         </NextLink>
         <NextLink href="/register">
           <Link mr={2}>Register</Link>
         </NextLink>
+      </>
+    );
+    // user is logged in
+  } else {
+    body = (
+      <>
+        <NextLink href="/">
+          <Link mr={2}>{data.me.username}</Link>
+        </NextLink>
+        <NextLink href="/logout">
+          <Link mr={2}>Logout</Link>
+        </NextLink>
+      </>
+    );
+  }
+
+  return (
+    <Flex bg="teal" p={4} width="100%">
+      <Box ml="auto" color="white">
+        {body}
       </Box>
     </Flex>
   );
