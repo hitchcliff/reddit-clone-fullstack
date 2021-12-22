138334e053007bb1fad3b68bd51352c6e927c520
kevin700brands <kevin@700brands.com>
2021-12-22 09:28:46 +0800

    Added Custom Pagination using URQL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,29 @@ import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 import NextLink from "next/link";
-import { Box, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 const Index = () => {
-  const [{ data, fetching }] = usePostsQuery({
-    variables: {
-      limit: 10,
-    },
+  const [variables, setVariables] = useState({
+    limit: 10,
+    cursor: null as null | string,
   });
+  const [{ data, fetching }] = usePostsQuery({
+    variables,
+  });
+
+  if (!fetching && !data) {
+    return <div>you have no query</div>;
+  }
 
   return (
     <>
@@ -23,11 +38,11 @@ const Index = () => {
             <NextLink href="/create-post">Create post</NextLink>
           </Link>
         </Flex>
-        {!data ? (
+        {!data && fetching ? (
           <div>loading...</div>
         ) : (
           <Stack spacing={8}>
-            {data.posts.map((p) => (
+            {data!.posts.map((p) => (
               <Box key={p.id} p={5} shadow="md" borderWidth="1px">
                 <Heading fontSize="xl">{p.title}</Heading>
                 <Text mt={4}>{p.textSnippet}</Text>
@@ -35,6 +50,22 @@ const Index = () => {
             ))}
           </Stack>
         )}
+        {data ? (
+          <Flex>
+            <Button
+              m="auto"
+              my={8}
+              onClick={() =>
+                setVariables({
+                  limit: 10,
+                  cursor: data.posts[data.posts.length - 1].createdAt,
+                })
+              }
+            >
+              Load more
+            </Button>
+          </Flex>
+        ) : null}
       </Wrapper>
     </>
   );
