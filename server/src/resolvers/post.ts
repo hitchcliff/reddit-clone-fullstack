7b3a970b7669277dfc4d4d7a42d94b8714b32316
kevin700brands <kevin@700brands.com>
2021-12-27 07:17:31 +0800

    A User can now change their Vote using Transacations query

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -16,6 +16,7 @@ import {
 import { MyContext } from "../types";
 import { isAuth } from "../middleware/isAuth";
 import { getConnection } from "typeorm";
+import { Updoot } from "../entities/Updoot";
 
 @InputType()
 class PostInput {
@@ -142,16 +143,55 @@ export class PostResolver {
     const isUpdoot = value !== -1;
     const realValue = isUpdoot ? 1 : -1;
 
-    await getConnection().query(
-      `
-      insert into updoot ("userId", "postId", value)
-      values (${userId}, ${postId}, ${realValue});
+    const updoot = await Updoot.findOne({
+      where: {
+        postId,
+        userId,
+      },
+    });
 
+    if (updoot && updoot.value !== realValue) {
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+      update updoot
+      set value  = $1
+      where "postId" = $2 and "userId" = $3
+        `,
+          [realValue, postId, userId]
+        );
+
+        console.log(updoot?.value, realValue);
+
+        await tm.query(
+          `
       update post
-      set points = points + ${realValue}
-      where id = ${postId};
-      `
-    );
+      set points = points + $1
+      where id = $2;
+          `,
+          [2 * realValue, postId]
+        );
+      });
+    } else if (!updoot) {
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+      insert into updoot ("userId", "postId", value)
+      values ($1, $2, $3);
+        `,
+          [userId, postId, realValue]
+        );
+
+        await tm.query(
+          `
+      update post
+      set points = points + $2
+      where id = $1;
+          `,
+          [postId, realValue]
+        );
+      });
+    }
 
     return true;
   }
